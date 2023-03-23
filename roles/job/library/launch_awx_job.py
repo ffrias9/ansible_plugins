dbcd9f3d50603e04eaa23f61e5ab86e4596fb47e
ffrias9 <framuxs@hotmail.com>
2023-03-23 08:56:41 +0100

    Versión 1.5 - Se han realizado mejoras.

--- roles/job/library/launch_awx_job.py
+++ roles/job/library/launch_awx_job.py
@@ -14,6 +14,7 @@ from ansible.module_utils._text import to_bytes, to_native
 def main():
     module_args = dict(
             url=dict(required=True, type='str'),
+            api_v=dict(required=True, type='str'),
             user=dict(required=True, type='str'),
             password=dict(required=True, type='str'),
             job=dict(required=True, type='str'),
@@ -26,26 +27,26 @@ def main():
             )
 
     auth = (module.params['user'], module.params['password'])
-    url = module.params['url'] + "/api/v2/job_templates/" + str(module.params['job']) + "/launch/"
+    url = module.params['url'] + "/api/v" + module.params['api_v'] + "/job_templates/" + module.params['job'] + "/launch/"
     credential = module.params['credential']
     payload = {"extra_vars": {}, "credentials": [credential]}
  
-    debg = (url, auth, payload)
     result = requests.post(url, auth=auth, json=payload)
 
     if result.status_code == 201:
-        noerror = True
-    else:
-        noerror = False
-
-    if noerror:
         result = dict(
             changed=True,
-            Response=f'Success launch job ' + str(module.params['job'])
+            Response=f'Success launch job ' + module.params['job']
             )
         module.exit_json(**result)
+    elif result.status_code == 401:
+        module.fail_json(msg=f'Error401 - Incorrect login credentials')
+    elif result.status_code == 400:
+        module.fail_json(msg=f'Error400 - Invalid request')
+    elif result.status_code == 404:
+        module.fail_json(msg=f'Error404 - Incorrect job id or api version')
     else:
-        module.fail_json(msg=f"{debg}")
+        module.fail_json(msg=f'Error launch job {result}')
 
 if __name__ == '__main__':
     main()
